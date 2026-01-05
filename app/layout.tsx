177dc09fc3bbeab7eae4af5c8908904192581929
NAGARAMPALLI SARVAN KUMAR <sarvankumarnagarampalli478@gmail.com>
2026-01-05 18:19:14 +0530

    Added OG Images Support

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,27 @@ const ubuntuFont = Ubuntu({
 export const metadata: Metadata = {
   title: "Stumps & Stories",
   description: "The OG Newsletter for Cricket by Sarvan Kumar",
+    openGraph: {
+    title: "Stumps & Stories",
+    description: "Automated Cricket Newsletter",
+    url: "https://stumps-and-stories.vercel.app/",
+    siteName: "Stumps & Stories",
+    images: [
+      {
+        url: "https://stumps-and-stories.vercel.app//og.png",
+        width: 1200,
+        height: 630,
+        alt: "Stumps & Stories - Automated Cricket Newsletter",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Stumps & Stories",
+    description: "Automated Cricket Newsletter",
+    images: ["https://stumps-and-stories.vercel.app/og.png"],
+  },
 };
 
 export default function RootLayout({
