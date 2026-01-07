d9526e5a0287a71604549504e323984af10429d6
NAGARAMPALLI SARVAN KUMAR <sarvankumarnagarampalli478@gmail.com>
2026-01-07 14:56:06 +0530

    Update route.ts

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -2,8 +2,4 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   return NextResponse.json({ ok: true });
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> a591a1aff8911fbb87f26bfe4d68547f1d790852
